4b359edbb7966ce957922ac4028dc8523776ec1e
GinChoYen <GinChoYen@hotmail.com>
2024-06-02 23:49:59 -0400

    Update Combine two tables.sql

--- leetcode_sql/Combine two tables.sql
+++ leetcode_sql/Combine two tables.sql
@@ -62,14 +62,14 @@ There is no address in the address table for the personId = 1 so we return null 
 addressId = 1 contains information about the address of personId = 2.
 
 
-Create table If Not Exists Person (personId int, firstName varchar(255), lastName varchar(255))
-Create table If Not Exists Address (addressId int, personId int, city varchar(255), state varchar(255))
-Truncate table Person
-insert into Person (personId, lastName, firstName) values ('1', 'Wang', 'Allen')
-insert into Person (personId, lastName, firstName) values ('2', 'Alice', 'Bob')
-Truncate table Address
-insert into Address (addressId, personId, city, state) values ('1', '2', 'New York City', 'New York')
-insert into Address (addressId, personId, city, state) values ('2', '3', 'Leetcode', 'California')
+Create table If Not Exists Person (personId int, firstName varchar(255), lastName varchar(255));
+Create table If Not Exists Address (addressId int, personId int, city varchar(255), state varchar(255));
+Truncate table Person;
+insert into Person (personId, lastName, firstName) values ('1', 'Wang', 'Allen');
+insert into Person (personId, lastName, firstName) values ('2', 'Alice', 'Bob');
+Truncate table Address;
+insert into Address (addressId, personId, city, state) values ('1', '2', 'New York City', 'New York');
+insert into Address (addressId, personId, city, state) values ('2', '3', 'Leetcode', 'California');
 
 
 -- Solution
